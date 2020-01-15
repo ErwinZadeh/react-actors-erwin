d5d5ebc3af06ff174b2ace740e2f7f0d61a8aa46
ErwinZadeh <farshidmzadeh@icloud.com>
2020-01-14 22:26:46 -0600

    Done

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Contacts:</h2>
+                <p>Cell No.</p>
+                <p>E-Mail</p>
+            </div>
+        );
+    }
+}
+export default Contact
+
